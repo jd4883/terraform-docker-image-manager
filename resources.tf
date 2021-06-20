b4d71b74443f23833e6e93f5f4c655c5b5c36a86
Jacob Dresdale <15719920+jd4883@users.noreply.github.com>
2021-06-19 23:42:17 -0700

    revised for more capabilities, build is not really well defined (yet)

--- resources.tf
+++ resources.tf
@@ -1,4 +1,7 @@
 resource "docker_image" "image" {
   name          = data.docker_registry_image.image.name
+  force_remove  = var.force_remove
+  keep_locally  = var.keep_locally
+  build         = var.build
   pull_triggers = [data.docker_registry_image.image.sha256_digest]
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,18 @@
-variable "image" {
-  type = string
+variable "image" { type = string }
+variable "build" { default = {} }
+
+variable "keep_locally" {
+  type    = bool
+  default = false
+}
+
+variable "force_remove" {
+  type    = bool
+  default = true
 }
 
 variable "tag" {
-  type = string
+  type    = string
   default = "latest"
 }
+
