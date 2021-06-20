66479687ab34898512ceca7382d77039d43f9423
Jacob Dresdale <15719920+jd4883@users.noreply.github.com>
2021-06-19 23:50:38 -0700

    revised for more capabilities, build is not really well defined (yet)

--- resources.tf
+++ resources.tf
@@ -2,6 +2,20 @@ resource "docker_image" "image" {
   name          = data.docker_registry_image.image.name
   force_remove  = var.force_remove
   keep_locally  = var.keep_locally
+  dynamic "build" {
+    for_each = var.build
+    # TODO: this segment can be better done without try
+    content {
+      build_arg = try(build.value.build_arg, {})
+      dockerfile = try(build.value.dockerfile, "Dockerfile")
+      force_remove = try(build.value.force_remove, true)
+      label = try(build.value.label, {})
+      no_cache = try(build.value.no_cache, true)
+      remove = try(build.value.remove, true)
+      tag = try(build.value.tag, [])
+      target = try(build.value.target, [])
+    }
+  }
   build         = var.build
   pull_triggers = [data.docker_registry_image.image.sha256_digest]
 }
